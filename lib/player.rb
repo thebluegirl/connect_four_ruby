e306f316e203bb304a74ea0d5d8c1d5a42107a45
Funto <funto.afuwape@gmail.com>
2023-11-22 14:45:48 +0100

    Create Player#add_slot
    All tests written for this method now pass.

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,12 @@
 class Player
+  attr_accessor :slots_played
   def initialize(player, symbol)
     @player = player
     @symbol = symbol
     @slots_played = []
+  end
+
+  def add_slot(cell)
+    @slots_played << cell
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,4 +1,19 @@
 require './lib/player'
-describe Player do
 
+describe Player do
+  describe '#add_slot' do
+    subject(:player) {described_class.new('Player', "\u26be")}
+    it 'increases the number of slots played' do
+      players_slots = player.instance_variable_get(:@slots_played)
+      cell = [1, 1]
+      expect{ player.add_slot(cell) }.to change{ players_slots.count }.by(1)
+    end
+
+    it 'adds the slot to the player instance object' do
+      players_slots = player.instance_variable_get(:@slots_played)
+      cell = [1, 1]
+      player.add_slot(cell)
+      expect(players_slots.include?(cell)).to be true
+    end
+  end
 end
