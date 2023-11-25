c8d9e3162ccbfb7ca5b62c1455e4a91ba91539db
Funto <funto.afuwape@gmail.com>
2023-11-25 15:31:42 +0100

    Create Player#column_win?
    This is to detect if a player has played a winning move in a column.
    
    All tests for this method in spec/player_spec.rb pass.

--- lib/player.rb
+++ lib/player.rb
@@ -17,19 +17,45 @@ class Player
         @slots_played.filter do |cell|
           cell.first == num
         end.sort
-      next if row.empty? || row.count < 4
+      next if row.count < 4
       columns = []
       row.each { |cell| columns << cell.last }
+
       def in_sequence?(array, columns)
         array.each do |number|
           return false if !columns.include?(number)
         end
         return true
       end
+
       return true if in_sequence?([1, 2, 3, 4], columns)
       return true if in_sequence?([2, 3, 4, 5], columns)
       return true if in_sequence?([3, 4, 5, 6], columns)
       return true if in_sequence?([4, 5, 6, 7], columns)
+    end
+    return false
+  end
+
+  def column_win?
+    1..7.times do |num|
+      column =
+        @slots_played.filter do |cell|
+          cell.last == num
+        end.sort
+      next if column.count < 4
+      rows = []
+      column.each { |cell| rows << cell.first }
+
+      def in_sequence?(array, rows)
+        array.each do |number|
+          return false if !rows.include?(number)
+        end
+        return true
+      end
+
+      return true if in_sequence?([1, 2, 3, 4], rows)
+      return true if in_sequence?([2, 3, 4, 5], rows)
+      return true if in_sequence?([3, 4, 5, 6], rows)
     end
     return false
   end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -26,9 +26,17 @@ describe Player do
         cells.each { |cell| slots_played << cell }
       end
 
-      it 'returns true' do
-        expect(player).to be_row_win
+      it { is_expected.to be_row_win }
+    end
+
+    context 'when player has played more than four on a row' do
+      before do
+        slots_played = player.instance_variable_get(:@slots_played)
+        cells = [[2, 2], [2, 3], [2, 4], [2, 5], [2, 6], [2, 7]]
+        cells.each { |cell| slots_played << cell }
       end
+
+      it {is_expected.to be_row_win}
     end
 
     context 'when player has not played up to four on any row' do
@@ -38,9 +46,68 @@ describe Player do
         cells.each { |cell| slots_played << cell }
       end
 
-      it 'returns false' do
-        expect(player).not_to be_row_win
+      it { is_expected.not_to be_row_win }
+    end
+
+    context 'when a player has not played up to four consequtively in a row' do
+      before do
+        slots_played = player.instance_variable_get(:@slots_played)
+        cells = [[1, 1], [1, 2], [1, 3], [1, 6], [2, 2], [2, 3], [2, 4]]
+        cells.each { |cell| slots_played << cell }
       end
+
+      it { is_expected.not_to be_row_win }
+    end
+
+    context 'when player has not played in any row' do
+      it {is_expected.not_to be_row_win}
+    end
+  end
+
+  describe '#column_win?' do
+    subject(:player) { described_class.new('Player', "\u26ab") }
+    context 'when player plays four in a column' do
+      before do
+        slots_played = player.instance_variable_get(:@slots_played)
+        cells = [[1, 1], [2, 1], [3, 1], [4, 1]]
+        cells.each { |cell| slots_played << cell }
+      end
+
+      it {is_expected.to be_column_win}
+    end
+
+    context 'when player plays more than four in a column' do
+      before do
+        slots_played = player.instance_variable_get(:@slots_played)
+        cells = [[2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3]]
+        cells.each { |cell| slots_played << cell }
+      end
+
+      it {is_expected.to be_column_win}
+    end
+
+    context 'when a player has not played up to four on any column' do
+      before do
+        slots_played = player.instance_variable_get(:@slots_played)
+        cells = [[1, 1], [2, 1], [3, 1], [2, 3], [3, 3], [4, 3]]
+        cells.each { |cell| slots_played << cell }
+      end
+
+      it {is_expected.not_to be_column_win}
+    end
+
+    context 'when a player has not played up to four in a column consequtively' do
+      before do
+        slots_played = player.instance_variable_get(:@slots_played)
+        cells = [[1, 1], [2, 1], [4, 1], [5, 1]]
+        cells.each { |cell| slots_played << cell }
+      end
+
+      it { is_expected.not_to be_column_win }
+    end
+
+    context 'when a player has not played any moves' do
+      it { is_expected.not_to be_column_win }
     end
   end
 end
