8430243c65a949ad4242a1c32630fc3bcc70231f
Funto <funto.afuwape@gmail.com>
2023-11-27 15:49:25 +0100

    Minor Corrections

--- lib/player.rb
+++ lib/player.rb
@@ -15,7 +15,8 @@ class Player
     return true if row_win?
     return true if column_win?
     return true if diagonal_win?
-    return false
+
+    false
   end
 
   def row_win?
@@ -25,22 +26,23 @@ class Player
           cell.first == num
         end.sort
       next if row.count < 4
+
       columns = []
       row.each { |cell| columns << cell.last }
 
-      def in_sequence?(array, columns)
+      in_sequence = lambda do |array, columns|
         array.each do |number|
-          return false if !columns.include?(number)
+          return false unless columns.include?(number)
         end
-        return true
+        true
       end
 
-      return true if in_sequence?([1, 2, 3, 4], columns)
-      return true if in_sequence?([2, 3, 4, 5], columns)
-      return true if in_sequence?([3, 4, 5, 6], columns)
-      return true if in_sequence?([4, 5, 6, 7], columns)
+      return true if in_sequence.call([1, 2, 3, 4], columns)
+      return true if in_sequence.call([2, 3, 4, 5], columns)
+      return true if in_sequence.call([3, 4, 5, 6], columns)
+      return true if in_sequence.call([4, 5, 6, 7], columns)
     end
-    return false
+    false
   end
 
   def column_win?
@@ -50,36 +52,37 @@ class Player
           cell.last == num
         end.sort
       next if column.count < 4
+
       rows = []
       column.each { |cell| rows << cell.first }
 
-      def in_sequence?(array, rows)
+      in_sequence = lambda do |array, rows|
         array.each do |number|
-          return false if !rows.include?(number)
+          return false unless rows.include?(number)
         end
-        return true
+        true
       end
 
-      return true if in_sequence?([1, 2, 3, 4], rows)
-      return true if in_sequence?([2, 3, 4, 5], rows)
-      return true if in_sequence?([3, 4, 5, 6], rows)
+      return true if in_sequence.call([1, 2, 3, 4], rows)
+      return true if in_sequence.call([2, 3, 4, 5], rows)
+      return true if in_sequence.call([3, 4, 5, 6], rows)
     end
-    return false
+    false
   end
 
   def diagonal_win?
-    def diagonal_cells_array(cell)
-      array = [[cell[0] + 1, cell[1] + 1], [cell[0] + 2, cell[1] + 2], [cell[0] + 3, cell[1] + 3]]
+    diagonal_cells_array = lambda do |cell|
+      [[cell[0] + 1, cell[1] + 1], [cell[0] + 2, cell[1] + 2], [cell[0] + 3, cell[1] + 3]]
     end
 
     @slots_played.each do |cell|
-      diagonal_array = diagonal_cells_array(cell)
+      diagonal_array = diagonal_cells_array.call(cell)
       if @slots_played.include?(diagonal_array[0]) && @slots_played.include?(diagonal_array[1]) && @slots_played.include?(diagonal_array[2])
         return true
       else
         next
       end
     end
-    return false
+    false
   end
 end
